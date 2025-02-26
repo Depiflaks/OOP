2b9d7266849c9f4b164de90b0fb5547f4583ab30
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-02-26 23:48:12 +0300

    lab-20-03 разделяем класс Bank на заголовок и реализацию

--- lab3/bank/main.cpp
+++ lab3/bank/main.cpp
@@ -1,7 +1,12 @@
+#include "src/bank/Bank.h"
 //
 // Created by smmm on 15.03.2025.
 //
-int main()
+int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		throw BankOperationError();
+	}
 	return 0;
 }

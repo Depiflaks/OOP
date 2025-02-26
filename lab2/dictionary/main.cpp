63e2b1c35848cc6e7019c8e22c94c1e270840bf0
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-02-26 23:48:12 +0300

    lab-13-03 инициализация 3 задания (словарь)

---
+++ lab2/dictionary/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+//
+// Created by smmm on 12.03.2025.
+//
+int main(int argc, char *argv[])
+{
+	std::string dictFile;
+	if (argc == 2)
+	{
+		dictFile = std::string(argv[1]);
+	}
+
+	return 0;
+}

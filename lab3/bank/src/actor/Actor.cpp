0a0346a65a8c2887f344dee76d2e08eca4b5b640
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-02-26 23:48:12 +0300

    lab-20-03 описание класса Actor + инициализация его методов

--- lab3/bank/src/actor/Actor.cpp
+++ lab3/bank/src/actor/Actor.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "Actor.h"
+
+Actor::Actor(Bank& bank, const Money cash)
+	: m_cashBalance(cash)
+	, m_accountBalance(0)
+	, m_accountId(std::nullopt)
+	, m_bank(bank)
+{
+}
+
+Money Actor::GetAccountBalance() const
+{
+}
+
+Money Actor::GetCashBalance() const
+{
+}
+
+std::optional<AccountId> Actor::GetAccountId() const
+{
+}
+
+void Actor::TransferMoney(Actor& dstActor, Money amount)
+{
+}
+
+void Actor::HandOverMoney(Actor& dstActor, Money amount)
+{
+}
+
+void Actor::ExtortMoney(Actor& dstActor, Money amount)
+{
+}
+
+void Actor::DepositMoney(Money amount)
+{
+}
+
+void Actor::WithdrawMoney(Money amount)
+{
+}
+
+void Actor::OpenAccount()
+{
+}
+
+void Actor::CloseAccount()
+{
+}
+
+void Actor::CheckActorHaveAccount(AccountId account) const
+{
+}
+
+void Actor::CheckActorHaveEnoughCash(AccountId account, Money amount) const
+{
+}
--- lab3/bank/src/actor/Actor.h
+++ lab3/bank/src/actor/Actor.h
@@ -5,14 +5,62 @@
 #ifndef ACTOR_H
 #define ACTOR_H
 
+#include "../bank/Bank.h"
 
+#include <optional>
 
-class Actor {
+class Actor
+{
 public:
+	explicit Actor(Bank& bank, Money cash);
+
+	// публичные методы:
+	// 1) узнать баланс на счёте
+	[[nodiscard]] Money GetAccountBalance() const;
+
+	// 2) узнать баланс в нале
+	[[nodiscard]] Money GetCashBalance() const;
+
+	// 3) узнать текущий id счёта в банке
+	[[nodiscard]] std::optional<AccountId> GetAccountId() const;
+
+	// 4) перевести другому актору на счёт
+	void TransferMoney(Actor& dstActor, Money amount);
+
+	// 5) передать деньги другому актору наличкой
+	void HandOverMoney(Actor& dstActor, Money amount);
+
+	// 6) вытрясти деньги у другого актора (наличка)
+	void ExtortMoney(Actor& dstActor, Money amount);
+
+	// 7) Пополнить собственный баланс
+	void DepositMoney(Money amount);
+
+	// 8) Вывести из собственного счёта
+	void WithdrawMoney(Money amount);
+
+	// 9) открыть Счёт
+	void OpenAccount();
+
+	// 10) закрыть счёт
+	void CloseAccount();
 
 private:
+	// параметры:
+	// - количество денег в нале
+	Money m_cashBalance;
+	// - количество денег на счету
+	Money m_accountBalance;
+	// - номер текущего счёта
+	std::optional<AccountId> m_accountId;
+	// - ссылка на банк
+	Bank& m_bank;
+
+	// приватные методы
+	// 1) проверить, есть ли у пользователя счёт в банке
+	void CheckActorHaveAccount(AccountId account) const;
+	// 2) проверить, достаточно ли у пользователя налички для того, чтобы поделиться
+	void CheckActorHaveEnoughCash(AccountId account, Money amount) const;
 };
 
-
-
-#endif //ACTOR_H
+#endif // ACTOR_H

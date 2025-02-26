65fdc0aba0825379587cd1138a944ff5d44dfeee
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-02-26 23:48:12 +0300

    lab-3-bank расширение логики города

--- lab3/bank/src/town/Town.cpp
+++ lab3/bank/src/town/Town.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Town.h"
+
+Town::Town(const Money startAmount)
+	: m_startAmount(startAmount)
+	, m_bank(Bank(startAmount))
+{
+	// поровну разделяем все деньги между владельцами
+	for (int i = CitizenName)
+}
+
+void Town::ExecuteSimulation(int stepCount)
+{
+}
+void Town::InitializingTheSpendingPlan()
+{
+}
+void Town::ExecuteSimulationStep()
+{
+}
--- lab3/bank/src/town/Town.h
+++ lab3/bank/src/town/Town.h
@@ -5,17 +5,34 @@
 #ifndef TOWN_H
 #define TOWN_H
 #include "../bank/Bank.h"
+#include "../citizen/description/map/citizenMap.h"
+
+#include <vector>
 
 class Town
 {
 public:
 	explicit Town(Money startAmount);
 	void ExecuteSimulation(int stepCount);
+	void InitializingTheSpendingPlan();
 
 private:
 	Money m_startAmount{ 0 };
+	CitizenMap m_citizens;
+	Bank m_bank;
+	std::vector<CitizenName> m_citizenOrder{
+		CitizenName::homerSimpson,
+		CitizenName::margeSimpson,
+		CitizenName::bartAndLisaSimpson,
+		CitizenName::apu,
+		CitizenName::mrBurns,
+		CitizenName::nelson,
+		CitizenName::snake,
+		CitizenName::waylonSmithers,
+	};
 
 	void ExecuteSimulationStep();
+	void CheckTotalAmount();
 };
 
 #endif // TOWN_H

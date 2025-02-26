bdfd531bc985a68ea40d905dc6b47243c3458136
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-02-26 23:48:12 +0300

    lab-20-03 ещё раз инициализируем класс Bank

---
+++ lab3/bank/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by smmm on 15.03.2025.
+//
+int main()
+{
+	return 0;
+}
---
+++ lab3/bank/src/bank/Bank.h
@@ -0,0 +1,16 @@
+//
+// Created by smmm on 15.03.2025.
+//
+
+#ifndef BANK_H
+#define BANK_H
+
+
+
+class Bank {
+
+};
+
+
+
+#endif //BANK_H

075e3b194096db5800ae94e8bfb8ed4c0d4b615d
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-05 09:28:10 +0300

    lab-3-bank методы для гомера

--- lab3/bank/src/citizen/citizens/citizenHomer.cpp
+++ lab3/bank/src/citizen/citizens/citizenHomer.cpp
@@ -3,6 +3,8 @@
 //
 #include "citizens.h"
 
+#include <Town.h>
+
 CitizenHomer::CitizenHomer(Bank& bank, IContactList& contacts, const Money cash)
 	: Citizen(bank, cash, CitizenName::homerSimpson, contacts)
 {
@@ -18,16 +20,35 @@ void CitizenHomer::TransferMoneyToMarge() const
 {
 	LogAboutPerformingAction("transferring money to Marge");
 	IContactList& contactList = GetContactList();
+
+	const auto marge = contactList.GetCitizen(CitizenName::margeSimpson);
+	const Money amountToMarge = GetRandomExpenseAmount();
+
+	TransferMoney(marge, amountToMarge);
 }
 
 void CitizenHomer::PayForElectricity() const
 {
 	LogAboutPerformingAction("paying for electricity");
 	IContactList& contactList = GetContactList();
+
+	const auto burns = contactList.GetCitizen(CitizenName::mrBurns);
+	const Money amountForElectricity = GetRandomExpenseAmount();
+
+	TransferMoney(burns, amountForElectricity);
 }
 
 void CitizenHomer::GiveMoneyToChildren()
 {
 	LogAboutPerformingAction("giving money to children");
 	IContactList& contactList = GetContactList();
+
+	auto bart = contactList.GetCitizen(CitizenName::bartSimpson);
+	auto lisa = contactList.GetCitizen(CitizenName::lisaSimpson);
+	const Money amountForChildren = GetRandomExpenseAmount();
+
+	WithdrawMoney(amountForChildren);
+
+	HandOverMoney(bart, amountForChildren / 2);
+	HandOverMoney(lisa, amountForChildren / 2);
 }

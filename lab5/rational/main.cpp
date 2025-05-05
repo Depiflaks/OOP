ff49842ecc74b2a2232276a30c02488f9c3685a3
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-06 02:43:03 +0300

    lab-5-rational инициализация задания

---
+++ lab5/rational/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by smmm on 06.05.2025.
+//
+
+int main()
+{
+}
---
+++ lab5/rational/src/cRational/CRational.h
@@ -0,0 +1,16 @@
+//
+// Created by smmm on 06.05.2025.
+//
+
+#ifndef CRATIONAL_H
+#define CRATIONAL_H
+
+
+
+class CRational {
+
+};
+
+
+
+#endif //CRATIONAL_H

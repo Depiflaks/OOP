3a9632a757ff9476ffb751edec8d7321b6511644
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-06 02:43:03 +0300

    lab-5-rational реализация базового класса

--- lab5/rational/src/cRational/CRational.cpp
+++ lab5/rational/src/cRational/CRational.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "CRational.h"
+
+CRational::CRational(const int value)
+	: m_numerator(value)
+{
+}
+
+CRational::CRational(const int numerator, const int denominator)
+	: m_numerator(numerator)
+	, m_denominator(denominator)
+{
+}
+
+int CRational::GetNumerator() const
+{
+	return m_numerator;
+}
+
+int CRational::GetDenominator() const
+{
+	return m_denominator;
+}
+
+double CRational::ToDouble() const
+{
+	return static_cast<double>(m_numerator) / static_cast<double>(m_denominator);
+}
--- lab5/rational/src/cRational/CRational.h
+++ lab5/rational/src/cRational/CRational.h
@@ -5,12 +5,36 @@
 #ifndef CRATIONAL_H
 #define CRATIONAL_H
 
+class CRational
+{
+public:
+	// Конструирует рациональное число, равное нулю (0/1)
+	CRational() = default;
 
+	// Конструирует рациональное число, равное value (value/1)
+	explicit CRational(int value);
 
-class CRational {
+	// Конструирует рациональное число, равное numerator/denominator
+	// Рациональное число должно храниться в нормализованном виде:
+	// знаменатель положительный (числитель может быть отрицательным)
+	// числитель и знаменатель не имеют общих делителей (6/8 => 3/4 и т.п.)
+	// Если знаменатель равен нулю, должно конструироваться рациональное число, равное нулю,
+	// либо должно быть выброшено исключение std::invalid_argument.
+	CRational(int numerator, int denominator);
 
+	// Возвращает числитель
+	[[nodiscard]] int GetNumerator() const;
+
+	// Возвращает знаменатель (натуральное число)
+	[[nodiscard]] int GetDenominator() const;
+
+	// Возвращает отношение числителя и знаменателя в виде числа double
+	[[nodiscard]] double ToDouble() const;
+
+	// Прочие операторы согласно заданию
+private:
+	int m_numerator{ 0 };
+	int m_denominator{ 1 };
 };
 
-
-
-#endif //CRATIONAL_H
+#endif // CRATIONAL_H

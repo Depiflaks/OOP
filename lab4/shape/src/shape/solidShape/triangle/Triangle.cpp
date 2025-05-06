87732933acd2e08a1dc6315ab455dacc80cfd93e
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-06 08:00:12 +0300

    lab-4-shape класс для треугольника

---
+++ lab4/shape/src/shape/solidShape/triangle/Triangle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smmm on 08.05.2025.
+//
+
+#include "Triangle.h"
---
+++ lab4/shape/src/shape/solidShape/triangle/Triangle.h
@@ -0,0 +1,21 @@
+//
+// Created by smmm on 08.05.2025.
+//
+
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+#include "Canvas/ICanvas.h"
+#include "shape/solidShape/ISolidShape.h"
+
+#include <string>
+
+class Triangle : ISolidShape
+{
+public:
+	double GetArea() override;
+	double GetPerimeter() override;
+	std::string ToString() override;
+	ColorType GetOutlineColor() override;
+};
+
+#endif // TRIANGLE_H

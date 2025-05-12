e32530f4a591e886f1b2bdc52e8e58db5203a589
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-12 21:49:49 +0300

    lab-4-shape моковый класс

---
+++ lab4/shape/src/canvas/mock/MockCanvas.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smmm on 12.05.2025.
+//
+
+#include "MockCanvas.h"
---
+++ lab4/shape/src/canvas/mock/MockCanvas.h
@@ -0,0 +1,22 @@
+//
+// Created by smmm on 12.05.2025.
+//
+
+#ifndef MOCKCANVAS_H
+#define MOCKCANVAS_H
+
+#include "../ICanvas.h"
+#include <gmock/gmock.h>
+
+class MockCanvas : public ICanvas {
+public:
+    MOCK_METHOD(void, DrawLine, (Point from, Point to, Color lineColor), (override));
+    MOCK_METHOD(void, DrawPolygon, (std::vector<Point> points, Color lineColor), (override));
+    MOCK_METHOD(void, FillPolygon, (std::vector<Point> points, Color fillColor), (override));
+    MOCK_METHOD(void, DrawCircle, (Point center, double radius, Color lineColor), (override));
+    MOCK_METHOD(void, FillCircle, (Point center, double radius, Color fillColor), (override));
+};
+
+
+
+#endif //MOCKCANVAS_H

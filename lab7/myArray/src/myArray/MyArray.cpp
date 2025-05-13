9f004a46d0278a00aff18d06c21175796a4db3e0
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-13 10:42:17 +0300

    lab-7-my-array класс MyArray

---
+++ lab7/myArray/src/myArray/MyArray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smmm on 13.05.2025.
+//
+
+#include "MyArray.h"
---
+++ lab7/myArray/src/myArray/MyArray.h
@@ -0,0 +1,12 @@
+//
+// Created by smmm on 13.05.2025.
+//
+
+#ifndef MY_ARRAY_H
+#define MY_ARRAY_H
+
+class MyArray
+{
+};
+
+#endif // MY_ARRAY_H

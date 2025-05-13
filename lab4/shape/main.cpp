2277734f53f8ce34efcbb7cb2f5aa2b033a781a6
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-13 10:00:21 +0300

    lab-4-shape доразнесли работу с openGl по функциям

--- lab4/shape/main.cpp
+++ lab4/shape/main.cpp
@@ -2,13 +2,26 @@
 // Created by smmm on 06.05.2025.
 //
 
+#include <canvas/openGL/GlCanvas.h>
 #include "shape/shapeReader/ShapeReader.h"
 #include "shape/solidShape/triangle/Triangle.h"
 
 #include <GLFW/glfw3.h>
-#include <canvas/openGL/GlCanvas.h>
 #include <functional>
 #include <iostream>
+
+constexpr int k_windowWidth = 800;
+constexpr int k_windowHeight = 600;
+auto k_windowCapture = "Picture";
+
+class OpenGlException final : public std::runtime_error
+{
+public:
+	OpenGlException()
+		: std::runtime_error("Error while trying to start OpenGl Unit")
+	{
+	}
+};
 
 void PrintShape(const std::shared_ptr<Shape>& shape)
 {
@@ -18,9 +31,6 @@ void PrintShape(const std::shared_ptr<Shape>& shape)
 		shape->Print(std::cout);
 }
 
-constexpr int k_windowWidth = 800;
-constexpr int k_windowHeight = 600;
-
 void InitOpenGl()
 {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -28,7 +38,7 @@ void InitOpenGl()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 }
 
-void StartDrawingLoop(GLFWwindow* window, const std::function<void()>& Draw)
+void DrawUntilExitWindow(GLFWwindow* window, const std::function<void()>& Draw)
 {
 	while (!glfwWindowShouldClose(window))
 	{
@@ -48,33 +58,52 @@ void StopDrawing(GLFWwindow* window)
 	glfwTerminate();
 }
 
-int main()
+void CheckOpenGlAvailable()
 {
-	const auto shapes = ShapeReader::ReadShapes(std::cin);
-
 	if (!glfwInit())
-		return -1;
+		throw OpenGlException();
+}
 
-	InitOpenGl();
-
-	GLFWwindow* window = glfwCreateWindow(k_windowWidth, k_windowHeight, "Picture", nullptr, nullptr);
-
+void CheckWindowCreationSuccessful(const GLFWwindow* window)
+{
 	if (!window)
 	{
 		glfwTerminate();
-		return -1;
+		throw OpenGlException();
 	}
+}
 
+void DrawPicture(const std::vector<std::shared_ptr<Shape>>& shapes)
+{
+	CheckOpenGlAvailable();
+	InitOpenGl();
+
+	GLFWwindow* window = glfwCreateWindow(k_windowWidth, k_windowHeight, k_windowCapture, nullptr, nullptr);
+	CheckWindowCreationSuccessful(window);
 	glfwMakeContextCurrent(window);
+
 	GlCanvas canvas;
 
 	const std::function DrawShapes{ [&shapes, &canvas] {
 		for (const auto& shape : shapes)
 			shape->Draw(canvas);
-	}};
+	} };
 
-	StartDrawingLoop(window, DrawShapes);
-
+	DrawUntilExitWindow(window, DrawShapes);
 	StopDrawing(window);
+}
+
+int main()
+{
+	const auto shapes = ShapeReader::ReadShapes(std::cin);
+	try
+	{
+		DrawPicture(shapes);
+	}
+	catch (OpenGlException& exception)
+	{
+		std::cout << exception.what() << '\n';
+		return 1;
+	}
 	return 0;
 }

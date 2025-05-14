5a6df31f2d37639fa7d36e4ce39f010b82dd263c
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-14 07:34:42 +0300

    lab6-http файл main.cpp

--- labs/lab6/http/main.cpp
+++ labs/lab6/http/main.cpp
@@ -3,12 +3,43 @@
 //
 
 #include "httpUrl/HttpUrl.h"
-
 #include <iostream>
-int main() {
-	int port = 65535;
-	HttpUrl url{"aboba.com", "/", Protocol::HTTP, port};
-	port = url.GetPort();
-	std::cout << port << std::endl;
+#include <string>
+
+void AnnotatePrintUrl(const HttpUrl& url)
+{
+	std::cout << "========================\n";
+	std::cout << "URL:       " << url.GetURL() << "\n";
+	std::cout << "Protocol:  " << ProtocolToString(url.GetProtocol()) << "\n";
+	std::cout << "Domain:    " << url.GetDomain() << "\n";
+	std::cout << "Port:      " << url.GetPort() << "\n";
+	std::cout << "Document:  " << url.GetDocument() << "\n";
+	std::cout << "========================\n";
+}
+
+void ProcessInputUrls()
+{
+	std::string line;
+	while (std::getline(std::cin, line))
+	{
+		try
+		{
+			const HttpUrl url(line);
+			AnnotatePrintUrl(url);
+		}
+		catch (const UrlParsingError& e)
+		{
+			std::cerr << "[Parsing Error] " << e.what() << "\n";
+		}
+		catch (...)
+		{
+			std::cerr << "[Unknown Error] while processing URL\n";
+		}
+	}
+}
+
+int main()
+{
+	ProcessInputUrls();
 	return 0;
 }

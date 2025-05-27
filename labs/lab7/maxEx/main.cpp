dc837d4a78742039e699eb8361da0971672cfe15
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-27 14:00:31 +0300

    lab-7-max файл main.cpp

--- labs/lab7/maxEx/main.cpp
+++ labs/lab7/maxEx/main.cpp
@@ -1,3 +1,20 @@
 //
 // Created by smmm on 27.05.2025.
 //
+
+#include "findMax.h"
+
+#include <iostream>
+#include <vector>
+int main()
+{
+	int n;
+	std::cin >> n;
+	std::vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		std::cin >> v[i];
+	int maxi;
+	FindMax(v, maxi, std::greater());
+	std::cout << maxi << std::endl;
+	return 0;
+}

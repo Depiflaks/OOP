faa718bf0a95f0ad39ae50e053941a8d6b6d3088
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-28 14:27:18 +0300

    lab-7-my-array инициализация класса, удаление лишних методов

--- labs/lab7/myArray/src/myArray/MyArray.h
+++ labs/lab7/myArray/src/myArray/MyArray.h
@@ -49,22 +49,26 @@ public:
 
 	~MyArray() noexcept
 	{
-		Clear();
-		operator delete[](m_data);
+		// TODO: деструктор
+		// Clear();
+		// operator delete[](m_data);
 	}
 
-	MyArray& operator=(MyArray other)
+	MyArray& operator=(MyArray& other)
 	{
-		swap(other);
+		// TODO: оператор копирования
+		return *this;
+	}
+
+	MyArray& operator=(MyArray&& other) noexcept
+	{
+		// TODO: оператор перемещения
 		return *this;
 	}
 
 	void PushBack(const value_type& value)
 	{
-		if (m_size == m_capacity)
-			grow();
-		new (m_data + m_size) T(value);
-		++m_size;
+		// TODO: оператор перемещения
 	}
 
 	[[nodiscard]] size_type GetSize() const noexcept { return m_size; }
@@ -85,26 +89,12 @@ public:
 
 	void Resize(const size_type newSize)
 	{
-		if (newSize > m_capacity)
-			reserve(newSize);
-		if (newSize > m_size)
-		{
-			for (size_type i = m_size; i < newSize; ++i)
-				new (m_data + i) value_type();
-		}
-		else
-		{
-			for (size_type i = newSize; i < m_size; ++i)
-				(m_data + i)->~T();
-		}
-		m_size = newSize;
+		// TODO: оператор resize
 	}
 
 	void Clear() noexcept
 	{
-		for (size_type i = 0; i < m_size; ++i)
-			(m_data + i)->~T();
-		m_size = 0;
+		// TODO: clar
 	}
 
 	iterator begin() noexcept { return m_data; }
@@ -122,42 +112,6 @@ private:
 	size_type m_size;
 	size_type m_capacity;
 
-	void reserve(const size_type newCapacity)
-	{
-		auto newData = static_cast<iterator>(operator new[](newCapacity * sizeof(T)));
-		uninitialized_move(m_data, m_data + m_size, newData);
-		Clear();
-		operator delete[](m_data);
-		m_data = newData;
-		m_capacity = newCapacity;
-	}
-
-	void grow()
-	{
-		const size_type newCap = m_capacity > 0 ? m_capacity * 2 : 1;
-		reserve(newCap);
-	}
-
-	void swap(MyArray& other) noexcept
-	{
-		std::swap(m_data, other.m_data);
-		std::swap(m_size, other.m_size);
-		std::swap(m_capacity, other.m_capacity);
-	}
-
-	static void uninitialized_copy(iterator srcBegin, iterator srcEnd, iterator dst)
-	{
-		iterator cur = dst;
-		for (; srcBegin != srcEnd; ++srcBegin, ++cur)
-			new (cur) T(*srcBegin);
-	}
-
-	static void uninitialized_move(iterator srcBegin, iterator srcEnd, iterator dst)
-	{
-		T* cur = dst;
-		for (; srcBegin != srcEnd; ++srcBegin, ++cur)
-			new (cur) T(std::move(*srcBegin));
-	}
 };
 
 #endif // MY_ARRAY_H

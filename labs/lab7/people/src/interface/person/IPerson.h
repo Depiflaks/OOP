32de9a6c50d88421413ae78a423896588025c7d5
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-29 04:56:12 +0300

    lab-7-people код для интерфейсов

--- labs/lab7/people/src/interface/person/IPerson.h
+++ labs/lab7/people/src/interface/person/IPerson.h
@@ -5,8 +5,39 @@
 #ifndef IPERSON_H
 #define IPERSON_H
 
-class IPerson
+#include <string>
+
+struct IPerson
 {
+	virtual ~IPerson() = default;
+	[[nodiscard]] virtual std::string GetFirstName() const = 0;
+	virtual void SetFirstName(const std::string&) = 0;
+	[[nodiscard]] virtual std::string GetLastName() const = 0;
+	virtual void SetLastName(const std::string&) = 0;
+	[[nodiscard]] virtual std::string GetPatronymic() const = 0;
+	virtual void SetPatronymic(const std::string&) = 0;
+	[[nodiscard]] virtual std::string GetAddress() const = 0;
+	virtual void SetAddress(const std::string&) = 0;
+};
+
+struct IPupil : public IPerson
+{
+	[[nodiscard]] virtual std::string GetSchoolName() const = 0;
+	virtual void SetSchoolName(const std::string&) = 0;
+	[[nodiscard]] virtual std::string GetGrade() const = 0;
+	virtual void SetGrade(const std::string&) = 0;
+};
+
+struct ITeacher : public IPerson
+{
+	[[nodiscard]] virtual std::string GetSubject() const = 0;
+	virtual void SetSubject(const std::string&) = 0;
+};
+
+struct IWorker : public IPerson
+{
+	[[nodiscard]] virtual std::string GetSpecialty() const = 0;
+	virtual void SetSpecialty(const std::string&) = 0;
 };
 
 #endif // IPERSON_H
--- labs/lab7/people/src/interface/student/IStudent.h
+++ labs/lab7/people/src/interface/student/IStudent.h
@@ -5,12 +5,20 @@
 #ifndef ISTUDENT_H
 #define ISTUDENT_H
 
+#include "interface/person/IPerson.h"
 
+#include <string>
 
-class IStudent {
-
+struct IStudent : public IPerson {
+    [[nodiscard]] virtual std::string GetUniversity() const = 0;
+    virtual void SetUniversity(const std::string&) = 0;
+    [[nodiscard]] virtual std::string GetStudentId() const = 0;
+    virtual void SetStudentId(const std::string&) = 0;
 };
 
-
+struct IAdvancedStudent : public IStudent {
+    [[nodiscard]] virtual std::string GetDissertationTopic() const = 0;
+    virtual void SetDissertationTopic(const std::string&) = 0;
+};
 
 #endif //ISTUDENT_H

bb345917be8b9e892ef6b45eb76c70eab9b00acd
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-29 05:02:33 +0300

    lab-7-people реализация StudentImpl

--- labs/lab7/people/src/implementation/StudentImpl.h
+++ labs/lab7/people/src/implementation/StudentImpl.h
@@ -4,13 +4,31 @@
 
 #ifndef STUDENTIMPL_H
 #define STUDENTIMPL_H
+#include <utility>
 
+#include "PersonImpl.h"
 
+template <typename Base>
+class StudentImpl : public CPersonImpl<Base>
+{
+public:
+	StudentImpl(const std::string& first, const std::string& last,
+		const std::string& patronymic, const std::string& address,
+		std::string university, std::string studentId)
+		: CPersonImpl<Base>(first, last, patronymic, address)
+		, m_university(std::move(university))
+		, m_studentId(std::move(studentId))
+	{
+	}
 
-class StudentImpl {
+	[[nodiscard]] std::string GetUniversity() const override { return m_university; }
+	void SetUniversity(const std::string& val) override { m_university = val; }
+	[[nodiscard]] std::string GetStudentId() const override { return m_studentId; }
+	void SetStudentId(const std::string& val) override { m_studentId = val; }
 
+private:
+	std::string m_university;
+	std::string m_studentId;
 };
 
-
-
-#endif //STUDENTIMPL_H
+#endif // STUDENTIMPL_H

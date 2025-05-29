8bd8004d90bb75e9662ad69ff2b4bd9fb0dc1d98
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-29 05:01:07 +0300

    lab-7-people частичная реализация PersonImpl

--- labs/lab7/people/src/implementation/PersonImpl.h
+++ labs/lab7/people/src/implementation/PersonImpl.h
@@ -5,12 +5,41 @@
 #ifndef PERSONIMPL_H
 #define PERSONIMPL_H
 
+#include "interface/person/IPerson.h"
 
+#include <string>
+#include <utility>
 
-class PersonImpl {
+template <typename Base>
+concept DerivedFromIPerson = std::is_base_of_v<IPerson, Base>;
 
+template <DerivedFromIPerson Base>
+class CPersonImpl : public Base
+{
+public:
+	CPersonImpl(std::string first, std::string last,
+		std::string patronymic, std::string address)
+		: m_firstName(std::move(first))
+		, m_lastName(std::move(last))
+		, m_patronymic(std::move(patronymic))
+		, m_address(std::move(address))
+	{
+	}
+
+	[[nodiscard]] std::string GetFirstName() const override { return m_firstName; }
+	void SetFirstName(const std::string& val) override { m_firstName = val; }
+	[[nodiscard]] std::string GetLastName() const override { return m_lastName; }
+	void SetLastName(const std::string& val) override { m_lastName = val; }
+	[[nodiscard]] std::string GetPatronymic() const override { return m_patronymic; }
+	void SetPatronymic(const std::string& val) override { m_patronymic = val; }
+	[[nodiscard]] std::string GetAddress() const override { return m_address; }
+	void SetAddress(const std::string& val) override { m_address = val; }
+
+private:
+	std::string m_firstName;
+	std::string m_lastName;
+	std::string m_patronymic;
+	std::string m_address;
 };
 
-
-
-#endif //PERSONIMPL_H
+#endif // PERSONIMPL_H

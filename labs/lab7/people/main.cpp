bc4635440ea2425adf33499548b90d047950f230
Smirnov Sergey <118541700+Depiflaks@users.noreply.github.com>
2025-05-29 05:29:05 +0300

    lab-7-people main.cpp

--- labs/lab7/people/main.cpp
+++ labs/lab7/people/main.cpp
@@ -1,8 +1,26 @@
 //
 // Created by smmm on 29.05.2025.
 //
+#include "implementation/advancedStudent/AdvancedStudent.h"
+#include "implementation/student/Student.h"
 
-int main()
-{
+#include <iostream>
 
+int main() {
+    Student student("Иван", "Иванов", "Иванович", "ул. Ленина, 1",
+                     "МГУ", "A12345");
+    student.SetAddress("ул. Пушкина, 10");
+    std::cout << "Студент: " << student.GetFirstName() << " "
+              << student.GetLastName() << ", Университет: "
+              << student.GetUniversity() << " (ID: "
+              << student.GetStudentId() << "), Адрес: "
+              << student.GetAddress() << std::endl;
+
+    AdvancedStudent grad("Петр", "Петров", "Петрович", "ул. Мира, 5",
+                          "СПбГУ", "B98765", "Искусственный интеллект");
+    std::cout << "Аспирант: " << grad.GetFirstName() << " "
+              << grad.GetLastName() << ", Тема диссертации: "
+              << grad.GetDissertationTopic() << std::endl;
+
+    return 0;
 }
